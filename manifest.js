60967fc789ed76e67bf02f8ce3d7ce2cdcfc8124
interngabriel <intern.gabriel@gli.id>
2025-03-24 16:12:53 +0700

    feat: add pwqa assets

---
+++ manifest.js
@@ -0,0 +1,24 @@
+
+const manifest = {
+    name: "Sistem Informasi Guru Absensi dan Presensi",
+    short_name: "SIGAP",
+    start_url: "/login",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  };
+  
+  export default manifest;
+  
