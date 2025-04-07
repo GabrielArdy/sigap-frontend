606f297ef1bc41546e9668752b319792f8613901
interngabriel <intern.gabriel@gli.id>
2025-04-07 14:14:18 +0700

    fix: add aprover comment to ui

--- src/app/admin/leave-request/[id]/page.jsx
+++ src/app/admin/leave-request/[id]/page.jsx
@@ -310,7 +310,7 @@ function LeaveRequestDetailPage() {
             </div>
             
             <div className="bg-white px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Nama Karyawan</dt>
+              <dt className="text-sm font-medium text-gray-500">Nama Guru</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{request.employee}</dd>
             </div>
             
